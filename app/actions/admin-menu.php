8f3e6e0b1e616e039418041502ec79bc16987606
Javier Troya <javier.troya@mediamechanics.com>
2023-10-30 11:46:49 +0100

    Add deployment.

--- app/actions/admin-menu.php
+++ app/actions/admin-menu.php
@@ -1,18 +1,18 @@
 <?php
 
 add_action('admin_menu', function(){
-    remove_menu_page('tools.php');
-    remove_menu_page('edit-comments.php');
-    remove_menu_page('plugins.php');
-    remove_submenu_page('edit.php', 'acf-settings-updates');
-    remove_submenu_page('themes.php', 'themes.php');
-    remove_submenu_page('themes.php', 'widgets.php');
-    remove_submenu_page('themes.php', 'customize.php');
-    remove_submenu_page('options-general.php', 'options-general.php');
-    remove_submenu_page('options-general.php', 'options-permalink.php');
-    remove_submenu_page('options-general.php', 'options-privacy.php');
-    remove_submenu_page('options-general.php', 'options-discussion.php');
-
-    global $submenu;
-    unset($submenu['themes.php'][6]);
+//    remove_menu_page('tools.php');
+//    remove_menu_page('edit-comments.php');
+//    remove_menu_page('plugins.php');
+//    remove_submenu_page('edit.php', 'acf-settings-updates');
+//    remove_submenu_page('themes.php', 'themes.php');
+//    remove_submenu_page('themes.php', 'widgets.php');
+//    remove_submenu_page('themes.php', 'customize.php');
+//    remove_submenu_page('options-general.php', 'options-general.php');
+//    remove_submenu_page('options-general.php', 'options-permalink.php');
+//    remove_submenu_page('options-general.php', 'options-privacy.php');
+//    remove_submenu_page('options-general.php', 'options-discussion.php');
+//
+//    global $submenu;
+//    unset($submenu['themes.php'][6]);
 }, 10, 0);
